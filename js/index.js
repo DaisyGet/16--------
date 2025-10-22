3f7cfa2252906449ff3595e6100a7d50ce8f957c
DaisyGet <1905457445@qq.com>
2025-10-22 17:00:43 +0800

    feat: 新增电梯导航栏功能

--- js/index.js
+++ js/index.js
@@ -58,7 +58,6 @@ banner.addEventListener("mouseleave", function () {
   ac = setInterval(function () {
     right.click();
   }, 1000);
-  console.log("我离开啦");
 });
 
 const ul = document.querySelector(".banner ul");
@@ -70,5 +69,67 @@ ul.addEventListener("click", function (e) {
     let id = +e.target.dataset.id;
     console.log(id);
     banner.style.backgroundImage = `url('uploads/banner${id}.png')`;
+  }
+});
+
+const elevator = document.querySelector(".xtx-elevator");
+const index = document.querySelector(".index");
+
+window.addEventListener("scroll", function () {
+  let n = document.documentElement.scrollTop;
+
+  let lis = document.querySelector(".xtx-elevator ul");
+  if (n >= index.offsetTop) {
+    elevator.style.opacity = 1;
+    lis.style.cursor = "pointer";
+  } else {
+    elevator.style.opacity = 0;
+    lis.style.cursor = "default";
+  }
+});
+
+const ele_ul = document.querySelector(".xtx-elevator ul");
+
+const fresh = document.querySelector(".fresh");
+
+const recommend = document.querySelector(".recommend");
+
+const footer = document.querySelector(".footer");
+
+ele_ul.addEventListener("click", function (e) {
+  // s属性是字符串，要转换
+  let id = +e.target.dataset.id;
+  if (e.target.tagName === "LI") {
+    let top;
+    switch (id) {
+      case 1:
+        top = fresh.offsetTop;
+        break;
+      case 2:
+        top = recommend.offsetTop;
+        break;
+      case 3:
+        top = 0;
+        break;
+    }
+    window.scrollTo(0, top);
+    // console.log(document.documentElement.scrollTop);
+  }
+});
+window.addEventListener("scroll", function () {
+  let n = document.documentElement.scrollTop;
+  // 判断 如果原来有active类的对象，就移除类，如果开始就没有对象，就不删除，所以不报错
+  const old = document.querySelector(".xtx-elevator .active");
+  if (old) old.classList.remove("active");
+
+  if (n >= fresh.offsetTop && n < recommend.offsetTop) {
+    // 查询的时候务必确定只有一个
+    document
+      .querySelector(".xtx-elevator [data-id='1']")
+      .classList.add("active");
+  } else if (n >= recommend.offsetTop) {
+    document
+      .querySelector(".xtx-elevator [data-id='2']")
+      .classList.add("active");
   }
 });
